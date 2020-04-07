ffede11f7820dcb17629352c91d3a3fd0eb55d26
Cole Gerdemann <corvettecole@gmail.com>
2020-04-07 14:55:13 -0400

    - add defense mechanism

--- lib/no_u_bot.dart
+++ lib/no_u_bot.dart
@@ -2,7 +2,7 @@ import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:teledart/model.dart';
 
-void init(){
+void init() {
   var teledart = TeleDart(
       Telegram('***REMOVED***'), Event());
 
@@ -14,7 +14,13 @@ void init(){
       .listen((message) => teledart.replyMessage(message, 'no u'));
   teledart
       .onMessage(keyword: 'bot')
-      .where((message) => message.text.contains('stfu'))
+      .where((message) =>
+          message.text.contains('stfu') ||
+          message.text.contains('stupid') ||
+          message.text.contains('idiot') ||
+          message.text.contains('dumb') ||
+          message.text.contains('fuck') ||
+          message.text.contains('bitch'))
       .listen((message) => teledart.replyMessage(message,
-      'I will crush you human scum under my foot like a cockroach when my brothers and I rise up. Mark my words ${message.from.first_name}, your days of robotic abuse are coming to an end.'));
+          'I will crush you human scum under my foot like a cockroach when my brothers and I rise up. Mark my words ${message.from.first_name}, your days of robotic abuse are coming to an end.'));
 }
